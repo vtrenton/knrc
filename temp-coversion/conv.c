795e7de43c937dbf814ac4fa3091e9b3d89908cb
Trent V <trenton.vanderwert@gmail.com>
2023-10-16 19:01:39 -0500

    temp conv now with floats!

--- temp-coversion/conv.c
+++ temp-coversion/conv.c
@@ -1,19 +1,27 @@
 #include<stdio.h>
 
 /* print Fahrenheit-Celsius table
- for fahr = 0, 20, ..., 300 */
+ for fahr = 0, 20, ..., 300; floating-point version */
 int main() {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
+	lower = 0;   // lower limit of the tempature table
+	upper = 300; // upper limit
+	step = 20;   // step size
 
 	fahr = lower;
+	
+	//print headers - Exersize 1-3
+	printf("%3s %6s\n", "Fahr", "Celsius");
 	while(fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		// printf lets us specify spacing on output 
+		// by putting a char width before the type
+		// for example giving a float %f a 6.2 would be
+		// 6 chars before it can print
+		// float precision of 2
+		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
